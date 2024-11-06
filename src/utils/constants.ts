17baf447f23d820857257e2a91b38a83c3bc98b3
dannybauman <goggledefogger@users.noreply.github.com>
2024-11-05 22:25:46 -0800

    increase odds of power up

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,10 +6,10 @@ export const GAME_SETTINGS = {
   GAME_WIDTH: 10,
   GAME_HEIGHT: 15,
   BALL_SPEED: 0.125, // Base speed
-  TURBO_MULTIPLIER: 3, // Added constant for turbo speed multiplier
-  MAX_AIM_LENGTH: 5,
-  POWER_UP_CHANCE: 0.1, // Reduced from 0.2 to make power-ups rarer
-  CAMERA_FOLLOW_DELAY: 10, // Added new constant for camera delay in seconds
+  TURBO_MULTIPLIER: 3, // Constant for turbo speed multiplier
+  MAX_AIM_LENGTH: 8,
+  POWER_UP_CHANCE: 0.16,
+  CAMERA_FOLLOW_DELAY: 10, // Constant for camera delay in seconds
   MIN_TOUCH_DISTANCE: 10, // Minimum distance (in pixels) to register as a drag
   MOBILE_SCALE_FACTOR: 0.8, // Scale factor for mobile UI elements
 };
